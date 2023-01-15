899bfc640616db5e4afd19e8ae3a6a38ae60c970
Samuel Carvalho <115167862+Rekyeme@users.noreply.github.com>
2023-01-15 13:33:23 +0000

    Creation of the Cannon and the Cannonball

--- base.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from main import SURFACE, Grey
-
-
-class Base:
-    def __init__(self):
-        pygame.draw.rect(SURFACE, Grey, (0, 260, 200, 280))
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pygame
 import sys
 from pygame.locals import *
 
+# General Setup
 pygame.init()
-SURFACE = pygame.display.set_mode((1000, 380))
-pygame.display.set_caption("Heavy Ordnance")
 clock = pygame.time.Clock()
+
+# Create the display surface
+window_height = 1000
+window_width = 380
+SURFACE = pygame.display.set_mode((window_height, window_width))
+pygame.display.set_caption("Heavy Ordnance")
 
 # Colors
 B_Blue = (173, 216, 230)
@@ -13,7 +18,11 @@ Grey = (121, 121, 121)
 Blue = (9, 129, 209)
 Danger = (207, 16, 32)
 
-# Game Loop Main
+
+# Sprites
+
+
+# Main Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -24,10 +33,20 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-    SURFACE.fill(B_Blue)    # Creates the game background;
-    pygame.draw.rect(SURFACE, Grey, (0, 260, 200, 280))     # Draws the base where the cannon is placed;
-    pygame.draw.rect(SURFACE, Danger, (200, 360, 140, 360))   # Draws the danger zone;
-    pygame.draw.rect(SURFACE, Blue, (340, 360, 1000, 200))  # Draws the sea;
+    # Creates the game background;
+    SURFACE.fill(B_Blue)
 
-    pygame.display.update()
+    # Draws the player area where the cannon is placed;(X, Y, WIDTH, HEIGHT)
+    pygame.draw.rect(SURFACE, Grey, [0, 200, 140, 180])
+
+    # Draws the danger zone; (X, Y, WIDTH, HEIGHT)
+    pygame.draw.rect(SURFACE, Danger, [140, 320, 140, 60])
+
+    # Draws the sea; (X, Y, WIDTH, HEIGHT)
+    pygame.draw.rect(SURFACE, Blue, [280, 320, 720, 60])
+
+    # Draws the cannon base in the player area; (X, Y, WIDTH, HEIGHT)
+    pygame.draw.rect(SURFACE, Blue, [40, 160, 60, 40])
+
+    pygame.display.flip()
     clock.tick(60)
